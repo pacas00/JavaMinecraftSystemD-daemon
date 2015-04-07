4ff11695a2525206450109dea6d0a865d1cd0820
Peter Cashel <pacas00@gmail.com>
2015-04-07 16:06:03 +1000

    Licence Header

--- src/main/java/net/petercashel/jmsDd/Configuration.java
+++ src/main/java/net/petercashel/jmsDd/Configuration.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Peter Cashel (pacas00@petercashel.net). All rights reserved.
+ * This program and the accompanying materials are made available under the terms
+ * of the Creative Commons Attribution-NoDerivatives 4.0 International License
+ * which accompanies this distribution, and is available at
+ * http://creativecommons.org/licenses/by-nd/4.0/.
+ *
+ * Contributors:
+ *     Peter Cashel - initial implementation
+ *******************************************************************************/
 package net.petercashel.jmsDd;
 
 import java.io.File;
