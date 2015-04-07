c6bf824cf5463d6aa22dcec1f6057ad63faa1e38
Peter Cashel <pacas00@gmail.com>
2015-04-07 17:09:05 +1000

    APL 2.0

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ This project contains the daemon code for the Java Minecraft SystemD daemon (JMS
 
 
 
+License
+=======
 
-<a rel="license" href="http://creativecommons.org/licenses/by-nd/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nd/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">JavaMinecraftSystemD-daemon</span> by <span xmlns:cc="http://creativecommons.org/ns#" property="cc:attributionName">Peter Cashel</span> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nd/4.0/">Creative Commons Attribution-NoDerivatives 4.0 International License</a>.
+This library is licensed under the Apache Software License, version 2.0.
--- src/main/java/net/petercashel/jmsDd/command/commandServer.java
+++ src/main/java/net/petercashel/jmsDd/command/commandServer.java
@@ -1,12 +1,17 @@
 /*******************************************************************************
- * Copyright (c) 2015 Peter Cashel (pacas00@petercashel.net). All rights reserved.
- * This program and the accompanying materials are made available under the terms
- * of the Creative Commons Attribution-NoDerivatives 4.0 International License
- * which accompanies this distribution, and is available at
- * http://creativecommons.org/licenses/by-nd/4.0/.
+ *    Copyright 2015 Peter Cashel (pacas00@petercashel.net)
  *
- * Contributors:
- *     Peter Cashel - initial implementation
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  *******************************************************************************/
 package net.petercashel.jmsDd.command;
 
